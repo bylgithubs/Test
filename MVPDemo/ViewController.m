0374b8c183117d5c9e1cffdcca44205040441902
廖宝裕 <1576660896@qq.com>
2019-07-16 09:11:48 +0800

    7

--- MVPDemo/ViewController.m
+++ MVPDemo/ViewController.m
@@ -25,7 +25,7 @@
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 	[self.tableview registerClass:[UITableViewCell class] forCellReuseIdentifier:NSStringFromClass([UITableViewCell class])];
-//111222333333444555555566666
+//111222333333444555555566666777777
 	self.presenter = [Presenter new];
 	[self.presenter attachView:self];
 	[self.presenter fetchData];
